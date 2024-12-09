dc0b3b7afee5e800f81ecc86273cadda5bfc0d0d
g3rry9898 <hydrogenbond36@gmail.com>
2024-12-09 00:11:00 -0800

    l

---
+++ utils/README.md
@@ -0,0 +1,16 @@
+# Redis Client
+
+This project contains a class to interact with Redis.
+
+## Files
+
+- `utils/redis.js`: Contains the `RedisClient` class.
+- `main.js`: Test file for the `RedisClient` class.
+
+## Usage
+
+To run the test, use the following command:
+
+```bash
+node main.js
+
---
+++ utils/redis.js
@@ -0,0 +1,45 @@
+#!/usr/bin/node
+/**
+ * RedisClient class to interact with Redis.
+ */
+
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (err) => {
+      console.error(`Redis client not connected to the server: ${err.message}`);
+    });
+
+    this.client.on('connect', () => {
+      console.log('Redis client connected to the server');
+    });
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return await this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    await this.delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
+
